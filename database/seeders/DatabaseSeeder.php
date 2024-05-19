e3b2f0966be482ef9454b48a6b83d827ab516d42
Robert Torres <robetorr311@gmail.com>
2024-05-19 17:12:35 -0400

    dulce paladar

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +16,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+        DB::table('comun.users')->insert(['name' => 'robetorr9962','email' => 'robetorr9962@gmail.com','password' => Hash::make('rt988311')]);
     }
 }
